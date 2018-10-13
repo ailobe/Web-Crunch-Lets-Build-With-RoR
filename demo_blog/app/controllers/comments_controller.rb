d6539255c62ee1fe1a605ef184372b944ee9dd41
ailobe <aitorlopezbeltran@gmail.com>
2018-10-13 23:41:32 +0200

    Finish Comments

---
+++ demo_blog/app/controllers/comments_controller.rb
@@ -0,0 +1,29 @@
+class CommentsController < ApplicationController
+    before_action :set_post
+    before_action :set_comment, only: [:destroy]
+
+
+    def create
+        @comment = @post.comments.create(comment_params)
+        redirect_to @post
+    end
+
+    def destroy
+        @comment.destroy
+        redirect_to @post
+    end
+
+    private
+        def comment_params
+            params.require(:comment).permit(:name,:content)
+        end
+
+        def set_post
+            @post = Post.find(params[:post_id])
+        end
+
+        def set_comment
+            @comment = Comment.find(params[:id])
+        end
+
+end
